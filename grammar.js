9b1a650acfbdca881447269ae63035bfcd9079ce
James Peach <jpeach6@bloomberg.net>
2024-08-03 16:50:02 +0100

    init commit

---
+++ grammar.js
@@ -0,0 +1,11 @@
+/// <reference types="tree-sitter-cli/dsl" />
+// @ts-check
+
+module.exports = grammar({
+  name: 'YOUR_LANGUAGE_NAME',
+
+  rules: {
+    // TODO: add the actual grammar rules
+    source_file: $ => 'hello'
+  }
+});
